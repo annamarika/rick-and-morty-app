e03877465e1bcf8c127653c3c49786104c5cfeca
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-04-08 18:57:40 +0200

    Add exercise one

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+let fetched = false;
+
+const apiURL = "https://rickandmortyapi.com/api/character";
+const container = document.querySelector("[data-js=container]");
+const button = document.createElement("div");
+
+button.innerHTML = `
+  <button class="character__button">Click here</button>
+  `;
+container.append(button);
+button.addEventListener("click", (event) => {
+  if (!fetched) {
+    fetchCharacters(apiURL);
+    fetched = !fetched;
+  }
+});
+
+async function fetchCharacters(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    characterList(data.results);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function characterList(characters) {
+  let list = document.createElement("ul");
+  list.className = "list";
+  button.append(list);
+
+  characters.forEach(({ name, image }) => {
+    const item = document.createElement("li");
+    item.className = "item";
+    item.innerHTML = `
+       <img src="${image}"/>
+      <p>${name}</p>
+    `;
+
+    list.append(item);
+  });
+}
