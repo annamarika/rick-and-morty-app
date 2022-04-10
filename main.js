908633e2542ac5c00cda621a7f279c6e9d13eb3d
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-04-10 22:45:30 +0200

    Add filter function and styling

--- main.js
+++ main.js
@@ -1,16 +1,59 @@
 import "./style.css";
 
 let fetched = false;
+let currentFilter = "all";
 
 const apiURL = "https://rickandmortyapi.com/api/character";
 const container = document.querySelector("[data-js=container]");
-const button = document.createElement("div");
+const containerCharacters = document.querySelector(
+  "[data-js=containerCharacters]"
+);
 
-button.innerHTML = `
-  <button class="character__button">Click here</button>
+container.innerHTML = `
+  <button class="character__button" data-js="button">Click here</button>
+  <form class= "form" data-js="filter-form" aria-labeledby="form-heading">
+  <fieldset class="form__fieldset">
+    <legend id="form-heading" class="form__legend">Filter by status:</legend>
+    <input
+      name="tag-filter"
+      value="all"
+      class="tag-input"
+      id="tag-all"
+      type="radio"
+      checked
+    />
+    <label class="tag-label" for="tag-all">all</label>
+    <input
+      name="tag-filter"
+      value="Alive"
+      class="tag-input"
+      id="tag-alive"
+      type="radio"
+    />
+    <label class="tag-label" for="tag-basic">alive</label>
+    <input
+      name="tag-filter"
+      value="Dead"
+      class="tag-input"
+      id="tag-dead"
+      type="radio"
+    />
+    <label class="tag-label" for="tag-css">dead</label>
+    <input
+      name="tag-filter"
+      value="unknown"
+      class="tag-input"
+      id="tag-unknown"
+      type="radio"
+    />
+    <label class="tag-label" for="tag-git">unknown</label>
+  </fieldset>
+</form>
   `;
-container.append(button);
-button.addEventListener("click", (event) => {
+
+const button = document.querySelector("[data-js=button]");
+
+button.addEventListener("click", () => {
   if (!fetched) {
     fetchCharacters(apiURL);
     fetched = !fetched;
@@ -26,20 +69,32 @@ async function fetchCharacters(url) {
     console.error(error.message);
   }
 }
-
+const filterForm = document.querySelector("[data-js=filter-form]");
 function characterList(characters) {
-  let list = document.createElement("ul");
+  const list = document.createElement("ul");
   list.className = "list";
-  button.append(list);
+  containerCharacters.append(list);
 
-  characters.forEach(({ name, image }) => {
-    const item = document.createElement("li");
-    item.className = "item";
-    item.innerHTML = `
-       <img src="${image}"/>
-      <p>${name}</p>
+  characters
+    .filter(
+      (character) =>
+        character.status.includes(currentFilter) || currentFilter === "all"
+    )
+    .forEach((character) => {
+      const item = document.createElement("li");
+      item.className = "item";
+      item.innerHTML = `
+       <img src="${character.image}"/>
+      <p class="character__name">${character.name}</p>
     `;
 
-    list.append(item);
-  });
+      list.append(item);
+    });
 }
+filterForm.addEventListener("change", () => {
+  containerCharacters.innerHTML = "";
+  currentFilter = filterForm.elements["tag-filter"].value;
+
+  const data = fetchCharacters(apiURL);
+  characterList(data);
+});
